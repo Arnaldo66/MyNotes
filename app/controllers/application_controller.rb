a90cc8c39b4c5fc14d2d3c4c3f160d1b26df85bc
Arnaud <a.parseval@gmail.com>
2016-10-28 05:28:01 -0700

    add active admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,20 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  protected
+
   def configure_permitted_parameters
-  	devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
-  	devise_parameter_sanitizer.permit(:account_update, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
+  def authenticate_admin_user!
+    redirect_to(new_user_session_path) unless current_user.try(:admin?)
+    authenticate_user!
+  end
+
+  def current_admin_user
+    return nil unless current_user.try(:admin?)
+    current_user
+  end
 end
